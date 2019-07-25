54f378e93a1c211d32dca181918fd0bf1ce1432d
Chris Cunningham <chris@intelatek.com>
2019-07-25 11:12:40 +0000

    Fix manager on pages helper and update home

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,36 +3,33 @@ module PagesHelper
   def calendly_link
     if current_user.group == "rs" || current_user.group == "ne" || current_user.group == "ls" || current_user.group == "ac" || 
       current_user.user_code == "rs" || current_user.user_code == "ne" || current_user.user_code == "ls" || current_user.user_code == "ac"
-      'Schedule a presentation for your client with an expert at Priority Commercial Payments: 
-      <a href="https://calendly.com/priority-greg/cpx" target="_New">https://calendly.com/priority-greg/cpx/a>'
+      '<li><a href="https://calendly.com/priority-greg/cpx" target="_New">Schedule a presentation with Greg McCarthy&#39;s Team</a></li>'
     elsif current_user.group == "se" || current_user.group == "cn" || current_user.group == "cc" || 
           current_user.user_code == "se" || current_user.user_code == "cn" || current_user.user_code == "cc"
-      'Schedule a presentation for your client with an expert at Priority Commercial Payments: 
-        <a href="https://calendly.com/priority-jason/cpx" target="_New">https://calendly.com/priority-jason/cpx</a>'
+      '<li><a href="https://calendly.com/priority-jason/cpx" target="_New">Schedule a presentation with Jason Arp&#39;s Team</a></li>'
     else
-      'Schedule a presentation for your client with an expert at Priority Commercial Payments:    
-      <li><a href="https://calendly.com/priority-greg/cpx" target="_New">Schedule a presentation with Greg McCarthy</a></li>
-      <li><a href="https://calendly.com/priority-jason/cpx" target="_New">Schedule a presentation with Jason Arp</a></li>'
+      '<li><a href="https://calendly.com/priority-greg/cpx" target="_New">Schedule a presentation with Greg McCarthy&#39;s Team</a></li>
+      <li><a href="https://calendly.com/priority-jason/cpx" target="_New">Schedule a presentation with Jason Arp&#39;s Team</a></li>'
     end
   end
 
   def my_manager
-    if (current_user.group || current_user.user_code) == "cc" 
-        'Chris Cunningham<br>chris@aspirepartners.co<br>(910) 528-1421'
-      elsif (current_user.group || current_user.user_code) == "rs" 
+    if current_user.group == "cc" || current_user.user_code == "cc" 
+        'Chris Cunningham<br>chris@aspirepartners.co<br>(800) 410-0233 Ext 500<br>Mobile (910) 528-1421'
+      elsif current_user.group == "rs" || current_user.user_code == "rs" 
         'Robert Sawyer<br>robert.sawyer@aspirepartners.co<br>(518) 424-8148'
-      elsif (current_user.group || current_user.user_code) == "ne" 
+      elsif current_user.group == "ne" || current_user.user_code == "ne" 
         'Neil Evans<br>neil.evans@aspirepartners.co<br>323-309-7225'                	  
-    	elsif (current_user.group || current_user.user_code) == "ls" 
+    	elsif current_user.group == "ls" || current_user.user_code == "ls" 
     	  'Laura Sardilli<br>laura.sardilli@aspirepartners.co<br>(703) 628-5816'
-    	elsif (current_user.group || current_user.user_code) == "ac" 
+    	elsif current_user.group == "ac" || current_user.user_code == "ac" 
     	  'Andy Charles<br>andy.charles@aspirepartners.co<br>(207)749-2631'
-    	elsif (current_user.group || current_user.user_code) == "se" 
+    	elsif current_user.group == "se" || current_user.user_code == "se" 
     	  'Stephanie Elad<br>stephanie.elad@aspirepartners.co<br>858-735-8904'              	  
-    	elsif  (current_user.group || current_user.user_code) == "cn" 
+    	elsif  current_user.group == "cn" || current_user.user_code == "cn" 
     	   'Chris Napier<br>chris.napier@aspirepartners.co<br>(717) 645-2413'
     	 else 
-    	   'You do not have an assigned manager.  Please contact admin for manager assignment: chris@aspirepartners.co'
+    	   'You do not have an assigned manager.  Please contact admin for manager assignment: admin@aspirepartners.co'
     end
   end
     
