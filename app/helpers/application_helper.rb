7a3d19ac94388ae971641467d38881126a917599
Chris Cunningham <chris@intelatek.com>
2019-08-14 23:54:36 +0000

    Add schedule SME as status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,5 +17,35 @@ module ApplicationHelper
 		@users.where(group: "td").count +
 		@users.where(group: "tz").count
   end
+
+  # Dashboard
+
+  def cps_all_closed
+    @prospects.where(hcs_status: "Closed - Client is Active").count +
+    @prospects.where(hcs_status: "Intro Presentation Given - No Sale").count +
+    @prospects.where(hcs_status: "SME Presentation Done - No Sale").count +
+    @prospects.where(hcs_status: "Proposal Given - No Sale").count +
+    @prospects.where(hcs_status: "Not Qualified").count +
+    @prospects.where(hcs_status: "Other - No Sale").count
+  end
+
+  def pay_all_closed
+    @prospects.where(hcs_status: "Closed - Client is Active").count +
+    @prospects.where(hcs_status: "Intro Presentation Given - No Sale").count +
+    @prospects.where(hcs_status: "SME Presentation Done - No Sale").count +
+    @prospects.where(hcs_status: "Not Qualified").count +
+    @prospects.where(hcs_status: "Other - No Sale").count
+  end
+
+  def hcs_all_closed
+    @prospects.where(hcs_status: "Closed - Group is Effective").count +
+    @prospects.where(hcs_status: "Intro Presentation Given - No Sale").count +
+    @prospects.where(hcs_status: "SME Presentation Done - No Sale").count +
+    @prospects.where(hcs_status: "Proposal Given - No Sale").count +
+    @prospects.where(hcs_status: "Not Qualified").count +
+    @prospects.where(hcs_status: "Other - No Sale").count
+  end
+
+
     
 end
