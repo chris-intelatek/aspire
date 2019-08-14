79948e8852c99a5332855852f5bd2b2ede06bb6f
Chris Cunningham <chris@intelatek.com>
2019-08-14 21:19:13 +0000

    add agent count and pie chart to dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@ module ApplicationHelper
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
     
+  def total_agents  
+    @users.where(group: "ajrc").count +
+  	@users.where(group: "ac").count +
+		@users.where(group: "cc").count +
+		@users.where(group: "cn").count +
+		@users.where(group: "ls").count +
+		@users.where(group: "ne").count +
+		@users.where(group: "rs").count +
+		@users.where(group: "se").count +
+		@users.where(group: "td").count +
+		@users.where(group: "tz").count
+  end
+    
 end
