f990629190db61c468fff892ac2687d9af57c547
Replit user <>
2023-05-25 14:37:46 +0000

    make app load on replit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  #Added this method to allow for display in frames
-  after_action :allow_intelatek_frames
-  
-  
+   
   def dashboard
     @prospects = current_user.my_prospects
     @users = User.my_users(current_user)
   end
- 
-  private
-  
-  def allow_intelatek_frames
-    response.headers['X-Frame-Options'] = 'ALLOWALL'
-  end
-  
 end
